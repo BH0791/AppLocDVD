0a0bea7610ef9c07372c0fd824cc5ec709cd06cd
HAMID <72698487+BH0791@users.noreply.github.com>
2023-01-18 10:22:42 +0100

    new -> activity_viewdvd.xml

---
+++ app/src/main/java/fr/hamtec/ViewDVDActivity.java
@@ -0,0 +1,12 @@
+package fr.hamtec;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewDVDActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+    }
+}
